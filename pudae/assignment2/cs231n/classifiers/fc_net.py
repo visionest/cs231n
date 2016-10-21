bug fix: use the square of standard deviation instead of the variance

pudae/assignment2/cs231n/classifiers/fc_net.py
@@ -3,6 +3,35 @@ import numpy as np
 from cs231n.layers import *
 from cs231n.layer_utils import *
 
+def affine_bn_relu_forward(x, w, b, gamma, beta, bn_param):
+  """
+  Convenience layer that perorms an affine transform followed by a ReLU
+
+  Inputs:
+  - x: Input to the affine layer
+  - w, b: Weights for the affine layer
+
+  Returns a tuple of:
+  - out: Output from the ReLU
+  - cache: Object to give to the backward pass
+  """
+  fc, fc_cache = affine_forward(x, w, b)
+  bn, bn_cache = batchnorm_forward(fc, gamma, beta, bn_param)
+  out, relu_cache = relu_forward(bn)
+  cache = (fc_cache, bn_cache, relu_cache)
+  return out, cache
+
+def affine_bn_relu_backward(dout, cache):
+  """
+  Backward pass for the affine-relu convenience layer
+  """
+  fc_cache, bn_cache, relu_cache = cache
+  drelu = relu_backward(dout, relu_cache)
+  dbn, dgamma, dbeta = batchnorm_backward(drelu, bn_cache)
+  dx, dw, db = affine_backward(dbn, fc_cache)
+
+  return dx, dw, db, dgamma, dbeta
+
 class TwoLayerNet(object):
   """
   A two-layer fully-connected neural network with ReLU nonlinearity and
@@ -184,10 +213,18 @@ class FullyConnectedNet(object):
     dims = [input_dim] + hidden_dims + [num_classes]
 
     for i, d in enumerate(zip(dims, dims[1:])):
-        kw = 'W{}'.format(i + 1)
-        kb = 'b{}'.format(i + 1)
+        idx = i + 1
+                           
+        kw = 'W{}'.format(idx)
+        kb = 'b{}'.format(idx)
         self.params[kw] = np.random.normal(scale=weight_scale, size=d)
         self.params[kb] = np.zeros(d[1])
+                           
+        if self.use_batchnorm:
+            kbn_gamma = 'bn_gamma{}'.format(idx)
+            kbn_beta = 'bn_beta{}'.format(idx)
+            self.params[kbn_gamma] = np.random.randn(d[1])
+            self.params[kbn_beta] = np.random.randn(d[1])
 
     ############################################################################
     #                             END OF YOUR CODE                             #
@@ -247,16 +284,24 @@ class FullyConnectedNet(object):
     # layer, etc.                                                              #
     ############################################################################
     params = self.params
+    bn_params = self.bn_params
     reg = self.reg
     num_layers = self.num_layers
 
     out = X
     cache = {}
     for i in xrange(1, num_layers):
-        kw = 'W{}'.format(i)
-        kb = 'b{}'.format(i)
-
-        out, cache[i] = affine_relu_forward(out, params[kw], params[kb])
+        W = params['W{}'.format(i)]
+        b = params['b{}'.format(i)]
+        
+        if self.use_batchnorm:
+            gamma = params['bn_gamma{}'.format(i)]
+            beta = params['bn_beta{}'.format(i)]
+            out, cache[i] = affine_bn_relu_forward(out, W, b,
+                                                   gamma, beta, bn_params[i - 1])
+        else:
+            out, cache[i] = affine_relu_forward(out, W, b)
+                           
         if self.use_dropout:
             out, cache['dropout{}'.format(i)] = dropout_forward(out, self.dropout_param)
 
@@ -301,8 +346,13 @@ class FullyConnectedNet(object):
         
         if self.use_dropout:
             dout = dropout_backward(dout, cache['dropout{}'.format(i)])
-            
-        dout, grads[kw], grads[kb] = affine_relu_backward(dout, cache[i])
+        
+        if self.use_batchnorm:
+            kbn_gamma = 'bn_gamma{}'.format(i)
+            kbn_beta = 'bn_beta{}'.format(i)
+            dout, grads[kw], grads[kb], grads[kbn_gamma], grads[kbn_beta] = affine_bn_relu_backward(dout, cache[i])
+        else:
+            dout, grads[kw], grads[kb] = affine_relu_backward(dout, cache[i])
         grads[kw] += reg * params[kw]
 
     ############################################################################

pudae/assignment2/cs231n/layers.py
@@ -171,15 +171,22 @@ def batchnorm_forward(x, gamma, beta, bn_param):
     # the momentum variable to update the running mean and running variance,    #
     # storing your result in the running_mean and running_var variables.        #
     #############################################################################
-    mean = np.mean(x, axis=0)
-    var = np.mean(np.square(x - mean), axis=0)
-    x_norm = (x - mean) / np.sqrt(var + eps)
+    
+    # numerator
+    mean = np.mean(x, axis = 0)
+    dev = x - mean
+    
+    # denominator
+    var = np.mean(np.square(dev), axis=0)  
+    std_dev = np.sqrt(var + eps)
+    
+    x_norm = dev / std_dev
     out = gamma * x_norm + beta
     
     running_mean = momentum * running_mean + (1 - momentum) * mean
     running_var = momentum * running_var + (1 - momentum) * var
     
-    cache = (x, mean, var, x_norm, gamma, beta)
+    cache = (x, gamma, beta, x_norm, dev, std_dev)
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
@@ -227,67 +234,45 @@ def batchnorm_backward(dout, cache):
   # TODO: Implement the backward pass for batch normalization. Store the      #
   # results in the dx, dgamma, and dbeta variables.                           #
   #############################################################################
-  x, mean, var, x_norm, gamma, beta = cache
+  
+  N, D = dout.shape
+    
+  x, gamma, beta, x_norm, dev, std_dev = cache
 
-  N = x.shape[0]
-  D = x.shape[1]
-
-  dev = x - mean                                          # (N, D)
-  std_dev = np.sqrt(var)                                  # (D,)
-
-  # out = gamma * x_norm + beta
+  # out = gamma * x_norm + beta                             
   dgamma = np.sum(dout * x_norm, axis=0)
   dbeta = np.sum(dout, axis=0)
-  dx_norm = dout * gamma                                  # (N, D)
+  dx_norm = dout * gamma
 
-  ###############################################################################
-  # Case i == j
-  ###############################################################################
-  # x_norm = (x - mean) / std_dev
-  #        = dev / std_dev
-  ddev = dx_norm / std_dev                                # (N, D)
-  dstd_dev = np.sum(dx_norm * -dev, axis=0) / var         # (N, D)
-    
-  ###############################################################################
-  # 1. numerator : dev (= (x - mean))
-  # dev = x - mean
-  dx = ddev * 1.0                                         # (N, D)
-  dx += -1.0 * np.mean(ddev, axis=0)                      # (N, D)
-
-  ###############################################################################
-  # 2. denominator : std_dev
-  # std_dev = np.sqrt(var)
-  dvar = dstd_dev / 2.0 / std_dev                         # (N, D)
-
-  # var = sum(square(dev), axis=0) / N
-  dvar /= N
-  
-  # square(dev) = dev ^ 2
-  ddev = dvar * 2.0 * dev                                 # (N, D)
-    
-  # dev = x - mean
-  dx += ddev * 1.0
-  dx += -1.0 * np.mean(ddev, axis=0)
- 
-  ###############################################################################
-  # Case i == j
-  ###############################################################################
-  # x_norm = (x - mean) / std_dev
-    
-  """
-  
   # x_norm = dev / std_dev
-  dstd_dev = dx_norm * -dev / var # var = std_dev ^ 2
-  dvar = dstd_dev / 2.0 / std_dev
-  # var = np.sum(np.square(dev) / N, axis=0)
-  dsquare_dev = dvar / N * ones_like(dev)
+  ddev = dx_norm / std_dev
+  dstd_dev = np.sum(dx_norm * -dev / np.square(std_dev), axis=0)
+  ############################################################################
+  # Question? Why this code does not work correctly?
+  # dstd_dev = np.sum(dx_norm * -dev / var, axis=0)
+    
+  # 1. numerator
   # dev = x - mean
-  """
+  dx = ddev
+  dmean = np.sum(ddev * -1, axis = 0)
+  dx += dmean / N
+    
+  # 2. denominator
+  # std_dev = sqrt(var)
+  dvar = dstd_dev / 2.0 / std_dev
+
+  # var = square(dev) / N
+  dvar = dvar / N
+  ddev = dvar * 2.0 * dev
   
+  # dev = x - mean
+  dx += ddev
+  dmean = np.sum(ddev * -1, axis = 0)
+  dx += dmean / N
+    
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
-
   return dx, dgamma, dbeta
 
 
@@ -313,7 +298,8 @@ def batchnorm_backward_alt(dout, cache):
   # should be able to compute gradients with respect to the inputs in a       #
   # single statement; our implementation fits on a single 80-character line.  #
   #############################################################################
-  pass
+  x, mean, var, x_norm, gamma, beta = cache
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################