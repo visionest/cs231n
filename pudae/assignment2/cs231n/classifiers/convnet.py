5bb6b8d750d1a43e7bf6935c6498b58cde90bbb8
pudae <pudae@estsoft.com>
2016-11-10 02:58:39 +0000

    Add spatial batchnorm

--- pudae/assignment2/cs231n/classifiers/convnet.py
+++ pudae/assignment2/cs231n/classifiers/convnet.py
@@ -36,7 +36,7 @@ class ConvNet(object):
     self.params[kW] = W
     self.params[kb] = b
     
-    self.forwards.append(lambda X: conv_relu_forward(X, self.params[kW], self.params[kb], conv_param))
+    self.forwards.append(lambda X, _: conv_relu_forward(X, self.params[kW], self.params[kb], conv_param))
     
     def backward(dout, cache):
         dout, dw, db = conv_relu_backward(dout, cache)
@@ -49,7 +49,7 @@ class ConvNet(object):
   def add_max_pool(self):
     pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
     
-    self.forwards.append(lambda X: max_pool_forward_fast(X, pool_param))
+    self.forwards.append(lambda X, _: max_pool_forward_fast(X, pool_param))
     self.backwards.insert(0, lambda dout, cache: (max_pool_backward_fast(dout, cache), {}))
     
     self.output_dim = (self.output_dim[0],
@@ -74,7 +74,7 @@ class ConvNet(object):
     self.params[kW] = W
     self.params[kb] = b
     
-    self.forwards.append(lambda X: affine_forward(X, self.params[kW], self.params[kb]))
+    self.forwards.append(lambda X, _: affine_forward(X, self.params[kW], self.params[kb]))
         
     def backward(dout, cache):
         dout, dw, db = affine_backward(dout, cache)
@@ -85,16 +85,37 @@ class ConvNet(object):
     self.output_dim = output_dim
     
   def add_relu(self):
-    self.forwards.append(lambda X: relu_forward(X))
+    self.forwards.append(lambda X, _: relu_forward(X))
     self.backwards.insert(0, lambda dout, cache: (relu_backward(dout, cache), {}))
     
   def add_affine_relu(self, output_dim):
     self.add_affine(output_dim)
     self.add_relu()
+    
+  def add_spatial_batchnorm(self, bn_param = {}):
+    idx = len(self.params) / 2 + 1
+    kgamma = 'bn_gamma{}'.format(idx)
+    kbeta = 'bn_beta{}'.format(idx)
+    self.params[kgamma] = np.ones(self.output_dim[0]).astype(self.dtype)
+    self.params[kbeta] = np.zeros(self.output_dim[0]).astype(self.dtype)
+    
+    def forward(X, mode):
+        param = bn_param
+        param['mode'] = mode
+        return spatial_batchnorm_forward(X, self.params[kgamma], self.params[kbeta], param)
+    
+    def backward(dout, cache):
+      dout, dgamma, dbeta = spatial_batchnorm_backward(dout, cache)
+      return dout, { kgamma: dgamma, kbeta: dbeta }
+    
+    self.forwards.append(forward)
+    self.backwards.insert(0, backward)
   
   def loss(self, X, y=None):
 
     scores = None
+    mode = 'test' if y is None else 'train'
+    X = X.astype(self.dtype)
     
     #############################################################################
     # forward pass
@@ -102,7 +123,7 @@ class ConvNet(object):
     out = X
     caches = []
     for f in self.forwards:
-        out, cache = f(out)
+        out, cache = f(out, mode)
         caches.append(cache)
     caches = reversed(caches)
     
